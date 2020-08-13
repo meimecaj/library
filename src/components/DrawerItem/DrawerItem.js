e66f9546418cb07a9ff2a26d1d5d820139bc996a
Franc <francbruzja@gmail.com>
2020-08-13 23:48:51 +0200

    fix reloading issue

--- src/components/DrawerItem/DrawerItem.js
+++ src/components/DrawerItem/DrawerItem.js
@@ -12,80 +12,73 @@ import LocalStorage from "../../services/LocalStorage";
 import { useLocation } from "react-router-dom";
 
 import useStyles from "./drawerItemStyle";
-import categories from "../../utils/categories";
 
 const localStorage = new LocalStorage();
+const allBooks = localStorage.get("books");
+
+const sortRoute = (pathname) => {
+  if (pathname.includes("-")) {
+    pathname = pathname.replace("-", "and");
+  } return allBooks[pathname];
+}
 
 const DrawerItem = (props) => {
   const classes = useStyles();
+
   const location = useLocation();
+  let pathname = location.pathname.split("/")[1];
 
-  const [category, setCategory] = useState([]);
+  const [category, setCategory] = useState(sortRoute(pathname));
 
-  useEffect(() => {
-    const books = localStorage.get("books");
-    let pathname = location.pathname.split("/")[1];
-
-    if (pathname.includes("-")) {
-      pathname = pathname.replace("-", "and");
-      setCategory(books[pathname]);
-    } else {
-      setCategory(books[pathname]);
-    }
-  }, category);
-
-  if (category.length > 0) {
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <main>
-          <div className={classes.heroContent}>
-            <Container maxWidth="sm">
-              <Typography
-                component="h3"
-                variant="h3"
-                align="center"
-                color="textPrimary"
-                gutterBottom
-              >
-                {props.title}
-              </Typography>
-            </Container>
-          </div>
-          <Container className={classes.cardGrid} maxWidth="md">
-            <Grid container spacing={4}>
-              {category.map((book) => (
-                <Grid item key={book} xs={12} sm={6} md={4}>
-                  <Card className={classes.card}>
-                    {/* <CardMedia
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <main>
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h3"
+              variant="h3"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              {props.title}
+            </Typography>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {category.map((book) => (
+              <Grid item key={book} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  {/* <CardMedia
                       className={classes.cardMedia}
                       image="https://source.unsplash.com/random"
                       title="Image title"
                     /> */}
-                    <CardContent className={classes.cardContent}>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {book.title}
-                      </Typography>
-                      <Typography noWrap>{book.description}</Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small" color="primary">
-                        View
-                      </Button>
-                      <Button size="small" color="primary">
-                        Edit
-                      </Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
-        </main>
-      </React.Fragment>
-    );
-  }
-  return <div></div>;
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {book.title}
+                    </Typography>
+                    <Typography noWrap>{book.description}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </React.Fragment>
+  );
 };
 
 export default DrawerItem;
